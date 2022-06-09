635529fdd03e0dd77f92cfea386c7cf381595643
Chaban Dmitriy <30317169+diman240798@users.noreply.github.com>
2022-06-09 12:48:03 +0300

    minor fix

--- TodoListUiKit/presentation/ui/CheckBox.swift
+++ TodoListUiKit/presentation/ui/CheckBox.swift
@@ -25,6 +25,7 @@ class CheckBox: UIButton {
     override func awakeFromNib() {
         self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
         self.isChecked = false
+        self.setTitle("", for: .normal)
     }
         
     @objc func buttonClicked(sender: UIButton) {
